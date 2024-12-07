c4aaae6e40487a8b9f98652bbed6da6fd8e3751d
Hector <hencinas23@gmail.com>
2024-12-07 12:37:46 +0100

    Hasta la acti 8

--- src/Tema3/Acti2/MyMath.java
+++ src/Tema3/Acti2/MyMath.java
@@ -31,4 +31,95 @@ public class MyMath {
     double area=PI*4*4;
     return area;
     }
+    public static boolean siPrimo(int num){
+        if(num<=1){
+            return false;
+        } else {
+            for (int i=2; i<num;i++){
+                if (num%i==0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean noPrimo(int num){
+        if(num<=1){
+            return true;
+        } else {
+            for (int i=2; i<num;i++){
+                if (num%i==0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static int numDigitos(int num){
+        int i=0;
+        if (num==0){
+            return 1;
+        }
+        for(i=0;num>0;i++){
+            num=num/10;
+        }
+        return i;
+    }
+    public static int numPares(int num){
+        int digitoPar=0;
+        if (num==0){
+            return 1;
+        }
+       while(num>0){
+            num=num/10;
+
+            if (num%2==0){
+                digitoPar++;
+            }
+        }
+        return digitoPar ;
+    }
+    public static int numImpares(int num){
+        int digitoImpar=0;
+        if (num==0){
+            return 0;
+        }
+        while(num>0){
+            num=num/10;
+
+            if (num%2!=0){
+                digitoImpar++;
+            }
+        }
+        return digitoImpar ;
+    }
+    public static int factorial(int num){
+        int total=1;
+        for(int i = 1; i<=num; i++){
+            total=total*i;
+        }
+        return total;
+    }
+    public static int factorialRecursiva(int num){
+        int total=1;
+        for(int i = num; i>=1; i--){
+            total=total*i;
+        }
+        return total;
+    }
+    public static int solucionesSegundoGrado(int a, int b, int c) {
+        int D = (b * b) - (4 * a * c);
+        if (D > 0) {
+            return 2;
+        } else if (D == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    public static int sumaDigitos(int num){
+        return 0;
+    }
+
+
 }
