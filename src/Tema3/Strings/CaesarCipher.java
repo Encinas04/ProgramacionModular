fb1654f84015b260984853612340edf021257ae9
hecencelb <hencinas23@gmail.com>
2025-01-23 13:46:09 +0100

    hecho

--- src/Tema3/Strings/CaesarCipher.java
+++ src/Tema3/Strings/CaesarCipher.java
@@ -1,5 +1,22 @@
 package Tema3.Strings;
 
-public class CaesarCipher {
+import java.util.Scanner;
 
+public class CaesarCipher {
+    public static void main(String[]args) {
+        Scanner scan=new Scanner(System.in);
+    System.out.println("Ingrese la frase");
+        String texto= scan.next();
+        encrypt(texto);
+
+    }
+    public static String encrypt(String texto){
+
+        for (int i=0;i<texto.length();i++){
+            char caracter=texto.charAt(i);
+            caracter+=1;
+        }
+return "";
+
+    }
 }
